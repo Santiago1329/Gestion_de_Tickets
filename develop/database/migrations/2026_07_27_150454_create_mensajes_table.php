d85059bd456fa4abaa7752d341bb52049ded6d97
Santiago <santiagogomez1129@gmail.com>
2026-07-27 15:12:28 -0500

    feat: agregar migración de tabla mensajes

---
+++ develop/database/migrations/2026_07_27_150454_create_mensajes_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('mensajes', function (Blueprint $table) {
+            $table->id();
+            $table->text('mensaje');
+            $table->timestamp('leido_at')->nullable();
+            $table->foreignId('ticket_id')->constrained('tickets')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->index(['ticket_id', 'created_at']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('mensajes');
+    }
+};
