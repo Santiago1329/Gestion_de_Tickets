d143b2f06780586a03e2b998e4410b5a776e674a
Santiago <santiagogomez1129@gmail.com>
2026-07-29 16:10:47 -0500

    feat: endpoint para guardar suscripción push del usuario

---
+++ develop/app/Http/Controllers/PushSubscriptionController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PushSubscriptionController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'endpoint' => 'required|string',
+            'keys.p256dh' => 'required|string',
+            'keys.auth' => 'required|string',
+        ]);
+
+        $request->user()->updatePushSubscription(
+            $request->endpoint,
+            $request->input('keys.p256dh'),
+            $request->input('keys.auth')
+        );
+
+        return response()->json(['ok' => true]);
+    }
+}
