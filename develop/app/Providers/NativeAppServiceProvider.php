58f21050076f63628b20b1b1a616ec2da3b11534
Santiago <santiagogomez1129@gmail.com>
2026-09-02 11:31:49 -0500

    feat: agregar NativePHP para generar el sistema como app de escritorio

---
+++ develop/app/Providers/NativeAppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Native\Desktop\Facades\Window;
+use Native\Desktop\Contracts\ProvidesPhpIni;
+
+class NativeAppServiceProvider implements ProvidesPhpIni
+{
+    /**
+     * Executed once the native application has been booted.
+     * Use this method to open windows, register global shortcuts, etc.
+     */
+    public function boot(): void
+    {
+        Window::open()
+        ->url('http://localhost:8000')
+        ->title('Gestion TICS')
+        ->width(1280)
+        ->height(800);
+    }
+
+    /**
+     * Return an array of php.ini directives to be set.
+     */
+    public function phpIni(): array
+    {
+        return [
+        ];
+    }
+}
