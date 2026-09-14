1b96a18f8a8d63f52b871d75fb9113a01d5eb884
Santiago <santiagogomez1129@gmail.com>
2026-09-14 15:08:23 -0500

    feat: agregar modelo Dispositivo y tabla de historial de checks

--- develop/app/Models/Dispositivo.php
+++ develop/app/Models/Dispositivo.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Dispositivo extends Model
 {
-    //
+    protected $fillable = [
+        'nombre',
+        'ip',
+        'sede',
+        'estado',
+        'ultimo_check_at'
+    ];
+
+    protected $casts = [
+        'ultimo_check_at' => 'datetime',
+    ];
+
+    public function checks()
+    {
+        return $this->hasMany(DispositivoCheck::class);
+    }
 }
