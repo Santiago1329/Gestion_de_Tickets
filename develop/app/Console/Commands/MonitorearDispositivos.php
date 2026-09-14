3a4321bbef3119b6e5deeadddec4ccf15ac16583
Santiago <santiagogomez1129@gmail.com>
2026-09-14 17:03:13 -0500

    feat: agregar notificacion DispositivoCaido y programar monitoreo cada 5 min

--- develop/app/Console/Commands/MonitorearDispositivos.php
+++ develop/app/Console/Commands/MonitorearDispositivos.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\Dispositivo;
 use App\Models\DispositivoCheck;
-
+use App\Notifications\DispositivoCaido;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Notification;
